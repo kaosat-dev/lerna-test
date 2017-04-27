d9aa95ec8074d71091937acdba4aa8798b8f42ad
Mark Moissette <kaosat.dev@gmail.com>
2017-04-27 11:26:29 +0200

    Foo (#2)
    * feat(newfeat): added new stuff

--- packages/second/index.js
+++ packages/second/index.js
@@ -1,3 +1,17 @@
 console.log('i am package-b')
 
-module.exports = {foo: 'sfsdfs'}
+/**
+ * someStuff - description
+ *
+ * @param  {type} bla description
+ * @param  {type} bli description
+ * @param  {type} blo description
+ * @return {type}     description 
+ */
+function someStuff (bla, bli, blo) {
+  console.log('i do only side effects')
+
+  return bla * 42
+}
+
+module.exports = {foo: 'sfsdfs', someStuff}
