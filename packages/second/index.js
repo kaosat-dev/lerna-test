fea6e97e13745e72011958b6a229b5d0c9be79bf
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-04-27 11:25:24 +0200

    docs(foo): added docs

--- packages/second/index.js
+++ packages/second/index.js
@@ -1,5 +1,13 @@
 console.log('i am package-b')
 
+/**
+ * someStuff - description
+ *
+ * @param  {type} bla description
+ * @param  {type} bli description
+ * @param  {type} blo description
+ * @return {type}     description 
+ */
 function someStuff (bla, bli, blo) {
   console.log('i do only side effects')
 
